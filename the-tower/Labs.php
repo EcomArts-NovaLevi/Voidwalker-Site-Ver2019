115f7f9d9cc2d6191e0cc47735572e3b9eadead7
Nova Levi <136199398+EcomArts-NovaLevi@users.noreply.github.com>
2023-06-10 16:46:01 -0500

    Add files via upload

---
+++ the-tower/Labs.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head><title>VOAU Bio: Mr. Nobody</title>
+<link href="../css/biocss.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+<style>
+	html {
+	background-image: url("../images/WebBackGSPACE.png");
+	background-repeat: no-repeat;
+	background-size: 100% auto;
+	overflow: hidden;
+}
+</style>
+</head>
+	<body>
+		<img id ="Pic" src="RemakeMrNobody.jpg" alt="The demidemon himself">
+		<h3><a href="https://voaudatabase.fandom.com/wiki/%22Mr._Nobody%22">Labsancthizar</a> the Demidemon</h3>
+		<div id="Info">
+			<p>
+				<em>You can't regret the past until you actually make it your past, so how do you realize the true feeling of lamentation before the deed is done?</em>
+			</p>
+			<p>
+				<br><br>
+				Not much is known of the character rather than what they do, not even their motive is clear to anyone.  The only link made of Mr. Nobody is to Alex, Aaron, and Nate. All three of them recall knowing him in memory despite that Alex is much younger than the other two. All three of them almost want to say he was a childhood friend, but their childhoods were at much different times in history and not all together.
+				<br>
+				<br>
+				Despite all inconsistencies and confusion he brings, everyone can agree on the fact that his real name is shortened down to them as "Labs". What they don't know is that Labs is short for Labsancthizar, a half-human half-demon exile from the layers of hell who takes his revenge by killing all demons that make it out and into World Zero. Ren made the acquaintance of Labs as a means of maybe finding rest for his wrath, but instead simply fueled it after realizing the futility. Labs would take his revenge the most off of Earth and as hell escaped throughout the Solar System like on the Moon and on Mars.
+				<br>
+				<br>
+				The reason for the strange familiarity people have to Labs has been theorized that he used certain kids around time and space as a means to cope with his situation and realize the good in humanity. To see not what these youth do but instead what they feel from their heart. Others seem to think that this might just be a trick, and that Labs would rather be ripping out demon's hearts instead.
+				<br>
+				<br>
+				<?php 
+					include("/home/stoutmartech/public_html/users/schoolera7861/major-arcana/includes/togbut.php");
+				?>
+			</p>
+		</div>
+		<div id="Biostuff">
+			<ul>
+				<li><strong>Age:</strong> ?</li>
+				<li><strong>Height:</strong> 5 ft, 10 in</li>
+				<li><strong>Birthday:</strong> October</li>
+				<li><strong>Place of Origin:</strong> ?</li>
+				<li><strong>Favorite Color:</strong> Grey</li>
+				<li><strong>Likes:</strong> Killing demons</li>
+				<li><strong>Dislikes:</strong> Seeing demons not dead yet</li>
+				<li><strong>General Behavior:</strong> Clandestine, introverted, reclusive, adamant</li>
+				<footer>
+					<?php 
+					include("/home/stoutmartech/public_html/users/schoolera7861/major-arcana/includes/footer.php");
+					?>
+				</footer>
+			</ul>
+		</div>
+	</body>
+</html>
---
+++ the-world/Katya.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html>
+<head><title>VOAU Bio: Avis Ardoris</title>
+<link href="../css/biocss.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+<style>
+	html {
+		background-image: url("DS3 Pyromancer Katya.jpg");
+		background-position: top;
+		background-repeat: no-repeat;
+		background-size: 100% auto;
+		background-attachment: fixed;
+		overflow: hidden;
+	}
+	.Warudo {
+		color: #d0def4;
+		clear: right;
+		font-family: Arial;
+		text-shadow: 3px 3px black;
+		border: rgba(0, 0, 0, 0.75);
+		background-color: rgba(0, 0, 0, 0);
+	}
+	span.Warudo:hover {
+		color: #210D28;
+	}
+</style>
+</head>
+	<body>
+		<img id ="Pic" src="LordofBirds.png" alt="Avis Ardoris, Katya">
+		<img id ="Pic2" src="missing.png" alt="Void World">
+		<h3><a href="https://voaudatabase.fandom.com/wiki/%22Katya%22">Avis Ardoris</a> and her <span class ="Warudo" onmouseover="openNav()" onmouseout="closeNav()">Void World</span></h3>
+		<script>
+			document.getElementById("Pic2").style.visibility = "hidden";
+			function openNav() {
+				document.getElementById("Pic2").style.width = "15%";
+				document.getElementById("Pic2").style.visibility = "visible";
+			}
+			function closeNav() {
+				document.getElementById("Pic2").style.width = "0%";
+				document.getElementById("Pic2").style.visibility = "hidden";
+			}
+		</script>
+		<div id="Info">
+			<p>
+				With the idea of looking at time as a wide spectrum, the end of it being visible not necessary, one can pinpoint Katya's birth to two places. In one instance she was born from an experiment by Dr. Simon Peterson to carry on his legacy after death and was developed in a lab, a process not somewhat known by everyone. Though it was to be in place of him, Simon thought instead to develop this person entirely differently as to not raise unnecessary suspicion or concern. The interesting shift was not to make a direct clone of Simon, but a merge of Aki, Ashton, and Alex's genes- to attempt to mix three people's genetic code where humans are only meant to have two direct person's genes; let alone having two sibling's genes merge with a third person's being complicated to warrant a success. After a surprising success, Katya was sent off as a newborn orphan in St. Petersburg, Russia.
+				<br><br>
+				Not long after abandonment, Katya was taken in by a married couple and was raised in Russia for five years before being sent off to Warsaw, Poland to a new family. However this time as an addition to a family and not to start one. Katya was only able to live in Poland for three years before more conflict followed her, and suspicions began to raise about her in her new family in America. She knew an impressive amount of Russian and Polish for her young age, and learned to speak English fluently as she stayed in Colorado until high school. For her freshman year Katya studied abroad in Nagoya, Japan, then spending her sophomore year in Leipzig, Germany. While there, she delved slightly into the Goth style and a partying, positive, and optimistic lifestyle. After returning home, Katya went to university in Copenhagen, Denmark, thinking about studying in the fields of bio-tech and engineering. Peers in university recalled her having a complete understanding of speaking, reading, and writing Russian, Polish, Danish, English, German, and Japanese. Beyond this, she also exhibited a well understanding of chemistry, astrology, epidemiology, physics, engineering, mathematics, and anatomy: exactly the same as what Simon is proficient in. In addition she is an amateur hacker and heavily understands history, claiming history be the favorite of them all.
+				<br><br>
+				Not even four years into studying in Denmark did Ashton and Ren catch wind of Katya's ongoing legacy, although her existence had been known for many years at that point, then soon following their discovery Aaron and Nate then got curious as well. Not one of them met with her nor knew what she looked like, despite Ren being an accomplished hacker- most notably that she hasn't found a way into Simon's residency either simply from his lack of visible network infrastructure or any leads to one. Instead she found Katya on social media or her records in education. Down past the rabbit hole she later found about her infamous skills and language proficiency. After this discovery, the Ashton concluded that she may as well have been sent to their group in mockery or flattery as Katya is, quite simply, every one of them combined as one. Aaron concluded that hypothesis similarly claiming that it was as if it were every one of them combined into Simon, evidence supported by the hard copies of his SCP file.
+				<br><br>
+				The second possible upbringing, and the one that is definitely true, is that Katya needed to create the people of this common society before they could create her first or else such a paradox would not be solved. While the above is true, her true upbringing being a child of the gods themselves in need of a human turned deity to rule mankind in said image of a person, it would later be ingrained into her being once the year of 2079 had passed officially on the linear timeline. She never existed any further as a child and instead of varying age ranges of 21 to 36 and appears to a set age from person to person, to anyone like Ashton, Ren, Kari, or Nate she would appear strictly 29. Why Katya looks like a mix of Ashton, Aki, and Alex is truthfully unknown. She was released to create the Cycle as a test of if she was worth the effort to, and successfully passed.
+				<br><br>
+				Known only by Kari, who's soul is in ownership by Katya and mentions from experience, Katya is bound to follow her wherever she it sent or simply taken to. Many inquiries by Kari mention Katya in times as early as the Egyptians and the Greeks as well as far forward into the 2700s. She also mentions Katya, involved or not, in separate realms of Earth known as Tamriel and the Aether meaning her jurisdiction follows the path of even a dimension hopper.
+				<br><br>
+				Katya in her godly nature knows all of what she rules over. Known in ancient times as Avis Ardoris, for her love of the birds and her carnal desires. With the birds is it loving and caring, as well as knowledgeable, of all avian kind. With revolvers it is quite different, it is more like already carrying the impressive dexterous skill that comes with gunslinging but not exactly having all revolvers at her disposal. That said, Katya had to make her way around various times and places to acquire one of every revolver ever made and get used to the feeling, as it were, while also already possessing the skill to flaunt them stylishly. Some revolvers in her travels impress her with their creativity, such as one that fires explosive slugs that detonate in a simultaneous cluster, one that's ghost haunts the weapon and supercharges it upon confirmed kills, a revolver user ability like an ability of her own, and a revolver crafted to be used from the hip without a sight picture. With all of this said, it does mean that she knows what's to be of the Cycle. When Katya mentions to others within it, "I know what's best for the Cycle." she really means that; Katya leans events one way or the other to perfectly balance the outcome as it was predestined to be. Those within it are helpless to this, and may experience events as if it were a miracle or divine intervention as to how things happen and this is an example of Katya stepping in. Whatever they do, it was already meant to be that way from the very beginning.
+				<br><br>
+				<?php 
+					include("/home/stoutmartech/public_html/users/schoolera7861/major-arcana/includes/togbut.php");
+				?>
+			</p>
+		</div>
+		<div id="Biostuff">
+			<ul>
+				<li><strong>Age:</strong> 22-32 (In Appearance)</li>
+				<li><strong>Height:</strong> 5 ft, 8 in</li>
+				<li><strong>Birthday:</strong> September 12th</li>
+				<li><strong>Favorite Color:</strong> Golden</li>
+				<li><strong>Likes:</strong> [ ? ]</li>
+				<li><strong>Dislikes:</strong> [ ? ]</li>
+				<li><strong>Personal Quote:</strong> "Inspiration is the lubricant of the gears that run the creative machine."</li>
+				<li><strong>General Behavior:</strong> Humorous, Persistent, Raunchy, Ambiguous</li>
+				<footer>
+					<?php 
+					include("/home/stoutmartech/public_html/users/schoolera7861/major-arcana/includes/footer.php");
+					?>
+				</footer>
+			</ul>
+		</div>
+	</body>
+</html>
